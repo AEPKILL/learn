43a540101e7fe05d7ba4acc8fcd39768062961ae
AEPKILL <a@aepkill.com>
2018-03-22 16:56:10 +0800

    Rxjs: 大概功能

--- font-end/rx.js/src/main.ts
+++ font-end/rx.js/src/main.ts
@@ -1,13 +1,36 @@
-import { testObservable } from './observable';
+import { Observable } from 'rxjs';
 
-class Work {
-  constructor() {
-    console.log('I work, whoohoo');
-  }
-}
+// 基本事件订阅 & 合并
+Observable.fromEvent(document.getElementById('add'), 'click')
+  .mapTo(1)
+  .merge(
+    Observable.fromEvent(document.getElementById('sub'), 'click').mapTo(-1)
+  )
+  .scan((total, now) => total + now, 0)
+  .subscribe(value => {
+    document.getElementById('view').innerText = `${value}`;
+  });
 
-// tslint:disable-next-line:no-unused-expression
-new Work();
+// flat Observable
+Observable.fromEvent(document, 'click')
+  .flatMapTo(Observable.of(1, 2, 3))
+  .subscribe(value => console.log(value));
 
-// tslint:disable-next-line:no-unused-expression
-new testObservable();
+// 可拖拽的 DOM 操作
+const dragDOM = document.getElementById('view');
+const mouseDown = Observable.fromEvent<MouseEvent>(dragDOM, 'mousedown');
+const mouseUp = Observable.fromEvent(document, 'mouseup');
+const mouseMove = Observable.fromEvent<MouseEvent>(document, 'mousemove');
+
+mouseDown
+  .map(event => ({ x: event.offsetX, y: event.offsetY }))
+  .flatMap(offset =>
+    mouseMove.takeUntil(mouseUp).map(event => ({
+      x: event.clientX - offset.x,
+      y: event.clientY - offset.y
+    }))
+  )
+  .subscribe(pos => {
+    dragDOM.style.left = `${pos.x}px`;
+    dragDOM.style.top = `${pos.y}px`;
+  });
