3096696ae31668087cd102fe2b815b9fb90978e0
AEPKILL <a@aepkill.com>
2018-11-26 13:24:34 +0800

    learn(typescript): union-types

--- typescript/advanced-types/src/union-types.ts
+++ typescript/advanced-types/src/union-types.ts
@@ -1,3 +1,8 @@
 // 联合类型
+type Union<T, U> = T | U;
+type Union2<T, U> = { [M in keyof (T | U)]: (T | U)[M] };
+
+type K1 = Union<{ name: string; p: string }, { age: number; p: number }>;
+type K2 = Union2<{ name: string; p: string }, { age: number; p: number }>;
 
 export default {};
