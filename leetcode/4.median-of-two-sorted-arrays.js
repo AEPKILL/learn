cd3d1a5fcf44ddf42e4d7e2d3701043df517bc06
AEPKILL <a@aepkill.com>
2019-02-13 15:33:10 +0800

    revert(leetcode): [4] Median of Two Sorted Arrays
    用空间换时间，减少函数调用，但是好像并没有明显的性能提升

--- leetcode/4.median-of-two-sorted-arrays.js
+++ leetcode/4.median-of-two-sorted-arrays.js
@@ -44,13 +44,15 @@
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
+ *
+ * 用空间换时间 140 - 144ms
  */
 var findMedianSortedArrays = function(nums1, nums2) {
   const lenN1 = nums1.length;
   const lenN2 = nums2.length;
   const median = Math.ceil((lenN1 + lenN2 + 1) / 2);
   const isOddLen = (lenN1 + lenN2) % 2 === 0;
-  const result = [0, 0];
+  const result = new Array(median);
 
   let i = 0; // point for nums1
   let j = 0; // point for nums2
@@ -58,25 +60,63 @@ var findMedianSortedArrays = function(nums1, nums2) {
   for (let k = 0; k < median; k++) {
     if (i < lenN1 && j < lenN2) {
       if (nums1[i] < nums2[j]) {
-        setResultNums(result, nums1[i++]);
+        result[i + j] = nums1[i++];
       } else {
-        setResultNums(result, nums2[j++]);
+        result[i + j] = nums2[j++];
       }
     } else if (i < lenN1) {
-      setResultNums(result, nums1[i++]);
+      result[i + j] = nums1[i++];
     } else if (j < lenN2) {
-      setResultNums(result, nums2[j++]);
+      result[i + j] = nums2[j++];
     }
   }
 
   if (isOddLen) {
-    return (result[0] + result[1]) / 2;
+    return (result[median - 1] + result[median - 2]) / 2;
   } else {
-    return result[1];
+    return result[median - 1];
   }
 };
 
-function setResultNums(nums, value) {
-  nums[0] = nums[1];
-  nums[1] = value;
-}
+// /**
+//  * @param {number[]} nums1
+//  * @param {number[]} nums2
+//  * @return {number}
+//  *
+//  * 140 - 144ms
+//  */
+// var findMedianSortedArrays = function(nums1, nums2) {
+//   const lenN1 = nums1.length;
+//   const lenN2 = nums2.length;
+//   const median = Math.ceil((lenN1 + lenN2 + 1) / 2);
+//   const isOddLen = (lenN1 + lenN2) % 2 === 0;
+//   const result = [0, 0];
+
+//   let i = 0; // point for nums1
+//   let j = 0; // point for nums2
+
+//   for (let k = 0; k < median; k++) {
+//     if (i < lenN1 && j < lenN2) {
+//       if (nums1[i] < nums2[j]) {
+//         setResultNums(result, nums1[i++]);
+//       } else {
+//         setResultNums(result, nums2[j++]);
+//       }
+//     } else if (i < lenN1) {
+//       setResultNums(result, nums1[i++]);
+//     } else if (j < lenN2) {
+//       setResultNums(result, nums2[j++]);
+//     }
+//   }
+
+//   if (isOddLen) {
+//     return (result[0] + result[1]) / 2;
+//   } else {
+//     return result[1];
+//   }
+// };
+
+// function setResultNums(nums, value) {
+//   nums[0] = nums[1];
+//   nums[1] = value;
+// }
