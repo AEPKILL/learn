e1f2619ee1f68304da5e94c0be5e7d0451d5b29e
AEPKILL <a@aepkill.com>
2019-02-18 12:30:37 +0800

    feat(leetcode): [23] Merge k Sorted Lists

---
+++ leetcode/23.merge-k-sorted-lists.js
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode id=23 lang=javascript
+ *
+ * [23] Merge k Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-k-sorted-lists/description/
+ *
+ * algorithms
+ * Hard (32.45%)
+ * Total Accepted:    334.5K
+ * Total Submissions: 1M
+ * Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+ *
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and
+ * describe its complexity.
+ *
+ * Example:
+ *
+ *
+ * Input:
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6
+ *
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+  const result = new ListNode(0);
+  let head = result;
+  let nodePointers = lists.slice(0);
+
+  while (true) {
+    // 存放需要去掉的空指针节点
+    const filterIndexs = [];
+
+    // 最小值节点的索引
+    let minPointerIndex = -1;
+    // 遍历所有的链表
+    for (let i = 0; i < nodePointers.length; i++) {
+      const node = nodePointers[i];
+      if (node === null) {
+        filterIndexs.push(i);
+        continue;
+      }
+      if (minPointerIndex === -1) {
+        minPointerIndex = i;
+      } else if (node.val < nodePointers[minPointerIndex].val) {
+        minPointerIndex = i;
+      }
+    }
+    if (minPointerIndex === -1) {
+      break;
+    }
+    head = head.next = nodePointers[minPointerIndex];
+    nodePointers[minPointerIndex] = nodePointers[minPointerIndex].next;
+
+    // 优化: 去掉空指针的节点
+    // 476ms -> 280ms
+    let j = 0;
+    for (const index of filterIndexs) {
+      nodePointers.splice(index - j, 1);
+      j++;
+    }
+  }
+
+  return result.next;
+};
