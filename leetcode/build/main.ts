2d692781b9cf02decd41828a48948e45559dd147
AEPKILL <a@aepkill.com>
2019-03-01 00:05:19 +0800

    feat(leetcode): [135] Candy

--- leetcode/build/main.ts
+++
@@ -1,9 +0,0 @@
-import { resolve } from 'path';
-import Builder from './utils/builder';
-
-const pwd = process.cwd();
-const root = resolve(pwd, './src');
-
-const builder = new Builder(root);
-builder.build();
-builder.watch();
