6177ded55433975332edc9ec57a17ed720e9a693
侯含 <yuanstop@gmail.com>
2020-01-06 11:26:35 +0800

    learn(InversifyJS): start

--- font-end/react/src/app.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RenderOpportunity from './components/render-opportunity';
-
-const App: React.SFC = () => {
-  return <RenderOpportunity />;
-};
-
-export default App;
